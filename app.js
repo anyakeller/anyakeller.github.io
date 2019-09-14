5f3024dce5dcd2075e1ed3315203e10cb724f108
Anya L Keller <kelleranyal@gmail.com>
2019-09-14 12:52:04 -0400

    whoops

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-$(document).foundation()
+$(document).foundation();
+$(".title-bar")
+    .on("sticky.zf.stuckto:top", function() {
+        $(this).addClass("shrink");
+    })
+    .on("sticky.zf.unstuckfrom:top", function() {
+        $(this).removeClass("shrink");
+    });
